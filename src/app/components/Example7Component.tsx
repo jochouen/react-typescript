86d526b2daff93b0e7e29e3bfab1bfd0597b5a58
chouen jo <xuzhangyuan0928@gmail.com>
2019-12-13 16:51:11 +0900

    react typescript test

--- src/app/components/Example7Component.tsx
+++ src/app/components/Example7Component.tsx
@@ -22,6 +22,51 @@ const Example7Component: React.FC = () => {
       <pre>
         <code>
           {`
+            import React, { createRef, forwardRef } from 'react';
+
+
+            const Example7Component: React.FC = () => {
+              const textInput = createRef<HTMLInputElement>();
+
+              const handleClick = () => {
+                if (textInput['current'].value.length > 0) {
+                  textInput['current'].select();
+                  document.execCommand('copy');
+                  alert('クリップボードにコピーしました:' + textInput['current'].value);
+                }else {
+                  alert('入力してください。');
+                }
+              }
+
+              return (
+                <div className="example">
+                  <h2>FC(Refs)</h2>
+                  <ChildrenComponent ref={textInput} onhandleClick={handleClick}/>
+                  <button onClick={handleClick}>COPY</button>
+                </div>
+              );
+            }
+
+            type Props = {
+              onhandleClick: VoidFunction
+            }
+
+            const ChildrenComponent = forwardRef<HTMLInputElement, Props>((props, ref) => {
+
+              const copyText = () => {
+                props.onhandleClick()
+              };
+
+              return (
+                <>
+                  <input type="text" ref={ref} />
+                  <button onClick={copyText}>コピー</button>
+                </>
+              )
+            })
+
+
+            export default Example7Component;
 
           `}
         </code>
